d21ceb058d25ed7357957f058f2b00338299b3d9
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-13 15:47:36 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,35 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', () => {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  /*
+  $().on( , () => {
+    
+  });
+  */
+}
+
+function pressIt() {
+  /*
+  $().on( , () => {
+    
+  });
+  */
+}
+
+function submitIt() {
+  /*
+  $().on( , () => {
+    
+  });
+  */
+}
+
 $(document).ready(function(){
 
 // call functions here
